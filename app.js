e0a331bcc31b81545840ce4f1f5605a2b2e76ce8
MRafiullah <mrafikhan2021@gmail.comvccgit config --global user.email mrafikhan2021@gmail.comgit config --global user.email mrafikhan2021@gmail.com>
2021-11-22 10:40:50 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,125 @@
+// SKIP 38 TO 42 DO WHILE ////SIR KE KAHA HAI
+
+
+//////CHAPTR NO 43 TO 52//////
+
+//Q NO 1//
+
+// function alex(){
+
+//     alert("Hello Sir")
+// }
+
+//Q NO 2//
+
+
+// function cov1(){
+// alert("Samsung S5")
+// }
+// function cov2(){
+//     alert("vivo S5")
+// }
+// function cov3(){
+//     alert("Samsung ipro")
+// }
+// function cov4(){
+//     alert("Samsung a3")
+// }
+
+//Q NO 3//
+// function del(){
+//     document.getElementById("name1").innerHTML=""
+  
+// }
+// function del1(){
+    
+//     document.getElementById("name2").innerHTML=""
+// }
+// function del2(){
+//     document.getElementById("name3").innerHTML=""
+    
+    
+// }
+// function del3(){
+//       document.getElementById("name4").innerHTML=""
+// }
+
+//Q NO 4//
+
+// function on(){
+//     document.getElementById('bulb').src="./img/on.jpg"
+// }
+// function off(){
+//     document.getElementById("bulb").src="./img/off.jpg"
+// }
+//Q NO 5//
+// var clicks = 0;
+// function incr(){
+    
+//     clicks += 1;
+//    var a = document.getElementById("incre").innerHTML = clicks;
+
+// }
+// var clicker = ;
+// function decre(){
+//     clicker -= ;
+//     document.getElementById("incre").innerHTML = clicker;
+// }
+//chap 49 -52
+//Q NO 1//
+// function submit(){
+//     var a = document.getElementById("MYPAR")
+//     var b = document.getElementById("MYNUM")
+//     document.write("name  = " +" "+a.value +"<br>" +"password  = " +" "+ b.value)
+// }
+
+//Q NO 1 2//
+// function reals(){
+//     var hmm = document.getElementById("realdoc")
+// hmm.innerHTML ="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque placeat dolores eaque.<a href =''>Hide<a/>"
+// }
+//Q NO 3//
+// function del(){
+//     document.getElementById("td1").innerHTML=""
+// }
+
+// function del1(){
+//     document.getElementById("td2").innerHTML=""
+// }
+// function del2(){
+//     document.getElementById("td3").innerHTML=""
+// }
+// function del3(){
+//     document.getElementById("td4").innerHTML=""
+
+// }
+// function gam(){
+// var user =prompt("Enter Your Name")
+// var user1 =prompt("Enter Your Class")
+// var user2 =prompt("Enter Your Position")
+
+//     document.getElementById("name").innerHTML=user
+//     document.getElementById("coll").innerHTML=user1
+//     document.getElementById("post").innerHTML=user2
+    
+// }
+// function gam1(){
+//     var user =prompt("Enter Your Name")
+//     var user1 =prompt("Enter Your class")
+//     var user2 =prompt("Enter Your Position")
+    
+//         document.getElementById("name1").innerHTML=user
+//         document.getElementById("coll2").innerHTML=user1
+//         document.getElementById("post3").innerHTML=user2
+        
+//     }
+//     function gam2(){
+//         var user =prompt("Enter Your Name")
+//         var user1 =prompt("Enter Your Class")
+//         var user2 =prompt("Enter Your Position")
+        
+//             document.getElementById("name2").innerHTML=user
+//             document.getElementById("coll5").innerHTML=user1
+//             document.getElementById("post6").innerHTML=user2
+            
+//         }
